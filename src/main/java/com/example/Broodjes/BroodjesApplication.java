7be8e615cf08a08886fa2e23876a55faaec4a144
Hamza Bachiri <h.bachiri@scia.net>
2021-11-29 19:17:34 +0100

    Initial commit

---
+++ src/main/java/com/example/Broodjes/BroodjesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Broodjes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroodjesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BroodjesApplication.class, args);
+	}
+
+}
